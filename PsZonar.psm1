935f23961e46c1ed26350ce8a860e2e017c648ad
Craig Buchanan <craibuc@gmail.com>
2016-11-07 14:55:10 -0800

    initial commit

---
+++ PsZonar.psm1
@@ -0,0 +1,25 @@
+##
+# load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
+#
+
+@("$PSScriptRoot\Functions\*.ps1") | Get-ChildItem | 
+    Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
+    % {
+
+        # dot-source script
+        Write-Debug "Loading $($_.BaseName)"
+        . $_
+
+        # export functions in the `Public` folder
+        if ( (Split-Path( $_.Directory) -Leaf) -Eq 'Public' ) {
+            Write-Debug "Exporting $($_.BaseName)"
+            Export-ModuleMember $_.BaseName
+        }
+
+    }
+
+##
+#  export aliases specified in a PS1 file
+#
+
+Export-ModuleMember -Alias jbustrip
